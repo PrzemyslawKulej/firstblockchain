4a18f8bd2b9bd25c128e67d2381d5cd00314a78f
PrzemyslawKulej <przemek.kulej1@gmail.com>
2023-09-18 23:57:05 +0200

    Working with endpoints in API.

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,24 @@
 import express, { Request, Response } from 'express';
+import bodyParser from "body-parser";
+import {Blockchain} from "./blockchain";
 
 const app = express();
 const PORT = 3000;
 
+const bitcoin = new Blockchain()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false }));
+
+//I can use here express.json() that is build in newer versions of Express
+
 
 app.get('/blockchain', function (req: Request, res: Response) {
+    res.send(bitcoin);
 
 });
 app.post('/transaction', function (req: Request, res: Response) {
+
 
 });
 app.get('/mine', function (req: Request, res: Response) {
@@ -15,5 +26,6 @@ app.get('/mine', function (req: Request, res: Response) {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on ${PORT}`);
+    console.log(`Server is live on this port ${PORT}`);
 });
+
